ea0bfc3b00430af9ae09e811ebb65d67b3dbbb91
0kalekale <kalekale.anon@gmail.com>
2011-02-19 10:56:27 +0545

    Minor stuff

--- src/hello.s
+++ src/hello.s
@@ -1,17 +1,14 @@
-        .global _start
-
-        .text
-_start:
-        # write(1, message, 13)
-        mov     $1, %rax                # system call 1 is write
-        mov     $1, %rdi                # file handle 1 is stdout
-        mov     $string, %rsi          # address of string to output
-        mov     $13, %rdx               # number of bytes
-        syscall                         # invoke operating system to do the write
-
-        # exit(0)
-        mov     $60, %rax               # system call 60 is exit
-        xor     %rdi, %rdi              # we want return code 0
-        syscall                         # invoke operating system to exit
+.global _start
+.text
+_start:        
+        mov     $1, %rax                
+        mov     $1, %rdi                
+        mov     $string, %rsi          
+        mov     $13, %rdx              
+        syscall                        
+        mov     $60, %rax               
+        xor     %rdi, %rdi              
+        syscall                         
 string:
-	.ascii  "Hello, world\n"
+	.ascii  "Do it for her.\n"
+
